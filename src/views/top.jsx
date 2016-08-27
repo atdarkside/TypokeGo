6bffc0c5ef34d123bcde2e22b3f5acd44ce3c3bf
reizou05 <reizou05.0h@gmail.com>
2016-08-27 18:13:13 +0900

    jsxに移植したみょすまままめみょりーん

--- src/views/top.jsx
+++ src/views/top.jsx
@@ -6,7 +6,12 @@ export default class Top extends React.Component {
     return (
       <section className="start-view">
         <div className="title-box">
-          <h1>TypokéGo</h1>
+          <h1>Typoké<span className="padd"></span>Go</h1>
+          <p>Type your beats.</p>
+          <span className="input-wra">
+            <input></input>
+            <i className="ion-search"></i>
+          </span>
         </div>
       </section>
     )
