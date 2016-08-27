b58ff864fc05ff99b54c71ed0af6c562e562c022
ghostrick <ghostrick.kinako@gmail.com>
2016-08-27 11:57:53 +0900

    環境揃えたり

--- api.js
+++ api.js
@@ -1,1 +1,42 @@
 // Express
+
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+
+//mongoose.connect('mongodb://localhost/TypokeGo');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3333;
+
+var router = express.Router();
+
+router.use(function(req, res, next) {
+    console.log('access -> ' + req.url);
+    next();
+});
+
+
+// ------------------------------ //
+//                                // 
+//            endpoit             //
+//                                //
+// ------------------------------ //
+
+
+router.get('/', function(req, res) {
+    res.json({ message: 'TypokeGo api server .' });
+});
+
+
+
+app.use('/api', router);
+app.use('/views',express.static('views'));
+
+
+app.listen(port);
+console.log('listen on port ' + port);
