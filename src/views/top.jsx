e84a543b009983a45dd824b23883d60e1c8733a5
wh11e7rue <wh11e7rue@icloud.com>
2016-08-28 06:57:12 +0900

    うぇぶぱっくしゅきい

--- src/views/top.jsx
+++ src/views/top.jsx
@@ -4,19 +4,23 @@ import {container} from '../utils'
 
 class Top extends React.Component {
   render() {
-    console.log(this.props.lyrics)
     return (
       <section className="start-view">
         <div className="title-box">
           <h1>Typoké<span className="padd"></span>Go</h1>
           <p>Type your beats.</p>
           <span className="input-wra">
-            <input></input>
+            <input placeholder="musicname"></input>
             <span className="candidate">candidate 1</span>
             <span className="candidate">candidate 2</span>
             <span className="candidate">candidate 3</span>
             <i className="ion-search"></i>
           </span>
+        </div>
+        <div className="action-link">
+          <div><i className="ion-log-in"></i></div>
+          <div><i className="ion-person-stalker"></i></div>
+          <div><i className="ion-person"></i></div>
         </div>
       </section>
     )
