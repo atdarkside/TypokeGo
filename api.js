23392240385f863c1263b0eb7697e1aa3fa556c7
TATaaa <pf35301@gmail.com>
2016-08-28 10:50:48 +0900

    all apis Complete

--- api.js
+++ api.js
@@ -11,6 +11,7 @@ var User = require('./models/user');
 var Score = require('./models/score');
 var Music = require('./models/music');
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -133,6 +134,7 @@ router.post('/music/save', function(req, res) {
     music.title = req.body.title;
     music.artists = req.body.artists;
     music.icon = req.body.icon;
+    music.youtube_url = req.body.youtube_url;
 
 	Music.find({ music_id:music.music_id }, function(err,_music){
     	if(err)
@@ -195,9 +197,71 @@ router.get('/music/search/keyword/:keyword', function(req, res) {
     			console.log(val.title);
     		}
     	});
-
     	res.json(sort);
     });
+});
+
+router.post('/music/initialization', function(req, res) {
+	var m001 = Music();
+	var m002 = Music();
+	var m003 = Music();
+	var m004 = Music();
+	var m005 = Music();
+
+	m001.music_id = 001;
+	m001.title = "Stand By Me";
+	m001.artists = "BEN E.KING";
+	m001.icon = "http://s.mxmcdn.net/images-storage/albums/1/1/8/5/8/8/11885811.jpg";
+	m001.youtube_url = "";
+
+	m001.music_id = 002;
+	m001.title = "Omen";
+	m001.artists = "Disclosure feat. Sam Smith";
+	m001.icon = "http://s.mxmcdn.net/images-storage/albums/4/1/5/7/4/0/32047514.jpg";
+	m001.youtube_url = "";
+
+	m001.music_id = 003;
+	m001.title = "Get lucky";
+	m001.artists = "Daft Punk";
+	m001.icon = "http://s.mxmcdn.net/images-storage/albums/5/7/5/5/3/4/26435575.jpg";
+	m001.youtube_url = "";
+
+	m001.music_id = 004;
+	m001.title = "RapGod";
+	m001.artists = "Eminem";
+	m001.icon = "http://s.mxmcdn.net/images-storage/albums8/1/3/8/1/8/6/31681831.jpg";
+	m001.youtube_url = "";
+
+	m001.music_id = 005;
+	m001.title = "ABCSong";
+	m001.artists = "Barney";
+	m001.icon = "http://s.mxmcdn.net/images-storage/albums/2/1/1/1/0/0/27001112.jpg";
+	m001.youtube_url = "";
+
+	m001.save(function(err){
+		if(err)
+			res.send(err);
+	});
+
+	m002.save(function(err){
+		if(err)
+			res.send(err);
+	});
+
+	m003.save(function(err){
+		if(err)
+			res.send(err);
+	});
+
+	m004.save(function(err){
+		if(err)
+			res.send(err);
+	});
+
+	m005.save(function(err){
+		if(err)
+			res.send(err);
+	});
 });
 
 app.use('/api', router);
